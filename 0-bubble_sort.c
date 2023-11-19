11c0cedf9ecac40d2b13719e9ee398bde0171d61
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-11-18 16:01:22 -0800

    Write a function that sorts an array of integers in ascending order using the Bubble sort algorithm
    Prototype: void bubble_sort(int *array, size_t size);
    You’re expected to print the array after each time you swap two elements (See example below)
    Write in the file 0-O, the big O notations of the time complexity of the Bubble sort algorithm, with 1 notation per line:
    
    in the best case
    in the average case
    in the worst case

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function that illustrates
+ * the bubble sort algorithm.
+ * @array: the data to be sorted.
+ * @size: the size of @array.
+ *
+ * Return: void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, swapped = 0;
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < (size - i - 1); ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swapped = 1;
+				array_swap(array, j, (j + 1));
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
+
+/**
+ * array_swap - A function to swap two arrays.
+ * @array: location where @l1, @l2 are stored.
+ * @l1: data to swap to the right.
+ * @l2: data to swap to the left.
+ *
+ * Return: void.
+ */
+void array_swap(int *array, int l1, int l2)
+{
+	int temp;
+
+	temp = array[l2];
+	array[l2] = array[l1];
+	array[l1] = temp;
+}
